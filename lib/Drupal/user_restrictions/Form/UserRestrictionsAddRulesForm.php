6c261ce1d2a31365435bc8f8d8e7d369cab04148
Adam Malone <adam@adammalone.net>
2014-04-28 13:23:37 +1000

    What can I say, huge commit that doesn't do a huge amount useable for the module: * Defined menu items/routing for UR * Created a basic admin form class for the admin page * Altered UR to become its very own entity :3 * Commented out a bunch of stuff that doesn't allow 8.x to work (stuff in install that looks for system table etc)

---
+++ lib/Drupal/user_restrictions/Form/UserRestrictionsAddRulesForm.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\user_restrictions\Form\UserRestrictionsAddRulesForm.
+ */
+
+namespace Drupal\user_restrictions\Form;
+
+use Drupal\Core\Form\FormBase;
+
+
+
+/**
+ * Provides a simple example form.
+ */
+class UserRestrictionsAddRulesForm extends FormBase {
+
+  /**
+   * Implements \Drupal\Core\Form\FormInterface::getFormID().
+   */
+  public function getFormID() {
+    return 'user_restrictions_add_rules_form';
+  }
+
+  /**
+   * Implements \Drupal\Core\Form\FormInterface::buildForm().
+   */
+  public function buildForm(array $form, array &$form_state) {
+    $form['user_restrictions_help'] = array(
+      '#type' => 'markup',
+      '#markup' => t("Set up rules for allowable usernames and e-mail address. A rule may either explicitly allow access or deny access based on the rule's Access type, Rule type, and Mask. If the username or e-mail address of an existing account or new registration matches a deny rule, but not an allow rule, then the account will not be created (for new registrations) or able to log in (for existing accounts)."),
+    );
+    $form['user_restrictions_help'] = array(
+      '#type' => 'markup',
+      '#markup' => t("Set up rules for allowable usernames and e-mail address. A rule may either explicitly allow access or deny access based on the rule's Access type, Rule type, and Mask. If the username or e-mail address of an existing account or new registration matches a deny rule, but not an allow rule, then the account will not be created (for new registrations) or able to log in (for existing accounts)."),
+    );
+    // Use the Form API to define form elements.
+    //return parent::buildForm($form, $form_state);
+    return $form;
+  }
+
+  /**
+   * Implements \Drupal\Core\Form\FormInterface::validateForm().
+   */
+  public function validateForm(array &$form, array &$form_state) {
+    //return parent::validateForm($form, $form_state);
+    // Validate the form values.
+  }
+
+  /**
+   * Implements \Drupal\Core\Form\FormInterface::submitForm().
+   */
+  public function submitForm(array &$form, array &$form_state) {
+    //parent::submitForm($form, $form_state);
+    //ubmitForm($form, $form_state);
+    // Do something useful.
+  }
+
+}
